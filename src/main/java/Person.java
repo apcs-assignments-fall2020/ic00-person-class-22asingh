8077a4eca4aa8eb618f873d8888992ded24f3cf2
22asingh <22asingh@athenian.org>
2020-10-26 16:25:47 +0000

    created Person class

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,39 @@
 public class Person {
-    
+    private String firstName;
+    private String lastName;
+
+    public Person(String a, String b){
+        firstName = a;
+        lastName = b;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public void setFirstName(String firstName){
+        if (firstName.trim().length() != 0){
+            this.firstName = firstName;
+        }
+    }
+
+    public void setLastName(String lastName){
+        if (lastName.trim().length() != 0){
+            this.lastName = lastName;
+        }
+    }
+
+    public void doSomething(){
+        System.out.println("I am watching Netflix");
+    }
+
+    public String toString(){
+        return firstName + " " + lastName;
+    }
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,8 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
+        Person p1 = new Person("Isabella","Smith");
+        System.out.println(p1.getFirstName());
+        p1.setFirstName("Bella");
+        System.out.println(p1.getFirstName());
     }
 }
